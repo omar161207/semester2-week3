ad30f101f7c89cde3b8e011bc0c11091a138cbb3
omar161207 <pvtg0844@leeds.ac.uk>
2026-02-14 18:12:24 +0000

    progress

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,37 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name:omar mohamed
+ * ID:201994442
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
-
+      double salary = 36250;
+      double NI_rate = 0.08;
+      double tax_rate = 0.15;
+      double NI_contribution = 0.0;
+      double tax_contribution = 0.0;
+      double taxable_income = 0.0;
+      double take_home_salary = 0.0;
     // calculate the deductions and final take-home salary
+      NI_contribution = salary * NI_rate;
+      taxable_income = salary - NI_contribution;
 
+      if (taxable_income <= 12500){
+        take_home_salary = taxable_income;
+      }
+      else{
+        tax_contribution = (taxable_income - 12500) * tax_rate;
+        take_home_salary = taxable_income - tax_contribution;
+      }
+    
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%.2f\n",salary);
+    printf("NI contribution £%.2f\n",NI_contribution);
+    printf("Tax contribution £%.2f\n",tax_contribution);
+    printf("Take home salary £%.2f\n",take_home_salary);
 
     return 0;
  }
